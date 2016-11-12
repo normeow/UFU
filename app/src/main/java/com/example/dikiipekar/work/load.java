32ef0914abe20591bfa10bb0029e723589244c74
GOTCODEHOOKED <qpv_1@mail.ru>
2016-11-12 21:07:24 +0300

    LEONID COMMIT

--- app/src/main/java/com/example/dikiipekar/work/load.java
+++ app/src/main/java/com/example/dikiipekar/work/load.java
@@ -1,5 +1,7 @@
 package com.example.dikiipekar.work;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -11,6 +13,13 @@ public class load extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.load);
 
-
+        new Handler().postDelayed(new Runnable() {
+                                      @Override
+                                      public void run() {
+                                          Intent i = new Intent(load.this, news.class);
+                                          startActivity(i);
+                                          finish();
+                                      }
+                                  },3*1000);
+        }
     }
-}
