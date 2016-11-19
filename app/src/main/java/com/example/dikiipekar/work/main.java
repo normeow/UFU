5597ea7dfe0141a8a0b65f029956d7763b615b80
LEONID-PC\Leonid <leonid220197@gmail.com>
2016-11-19 06:30:27 +0400

    PUSH PUSH я сделаль

--- app/src/main/java/com/example/dikiipekar/work/main.java
+++ app/src/main/java/com/example/dikiipekar/work/main.java
@@ -3,8 +3,12 @@ package com.example.dikiipekar.work;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 import com.aurelhubert.ahbottomnavigation.AHBottomNavigation;
 import com.aurelhubert.ahbottomnavigation.AHBottomNavigation.OnTabSelectedListener;
@@ -13,23 +17,28 @@ import com.example.dikiipekar.work.Fragments.Job;
 import com.example.dikiipekar.work.Fragments.News;
 import com.example.dikiipekar.work.Fragments.Stock;
 
-
+import static com.example.dikiipekar.work.R.id.fragment_job;
+import static com.example.dikiipekar.work.R.id.fragment_news;
+import static com.example.dikiipekar.work.R.id.fragment_stock;
+import static com.example.dikiipekar.work.R.id.infoTextView;
 
 
 public class Main extends AppCompatActivity implements OnTabSelectedListener {
 
-        AHBottomNavigation bottomNavigation;
+       private AHBottomNavigation bottomNavigation;
+        private TextView infoTextView;
 
 @Override
 protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
+        /*Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);*/
+        infoTextView = (TextView) findViewById(R.id.infoTextView);
         bottomNavigation= (AHBottomNavigation) findViewById(R.id.myBottomNavigation_ID);
         bottomNavigation.setOnTabSelectedListener(this);
         this.createNavItems();
+
         }
 
 private void createNavItems()
@@ -62,15 +71,19 @@ public void onTabSelected(int position, boolean wasSelected) {
         {
         News newsFragment = new News();
         getSupportFragmentManager().beginTransaction().replace(R.id.content_id,newsFragment).commit();
-
+                infoTextView.setText(R.string.news);
         }else  if (position==1)
         {
         Job jobFragment=new Job();
         getSupportFragmentManager().beginTransaction().replace(R.id.content_id,jobFragment).commit();
+                infoTextView.setText(R.string.job);
         }else  if (position==2)
         {
         Stock stockFragment=new Stock();
         getSupportFragmentManager().beginTransaction().replace(R.id.content_id,stockFragment).commit();
+                infoTextView.setText(R.string.stock);
         }
      }
+
+
 }
