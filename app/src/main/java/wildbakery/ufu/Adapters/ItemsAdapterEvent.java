4e01e6409d17f2b35c8a14076de04f45d5dc213c
GOTCODEHOOKED <qpv_1@mail.ru>
2017-04-20 17:21:54 +0400

    fix itemsAdapterEvent

--- app/src/main/java/wildbakery/ufu/Adapters/ItemsAdapterEvent.java
+++ app/src/main/java/wildbakery/ufu/Adapters/ItemsAdapterEvent.java
@@ -22,9 +22,13 @@ import wildbakery.ufu.R;
 
 
 
-public class ItemsAdapterEvent extends RecyclerView.Adapter<ItemsAdapterEvent.ViewHolder> {
+public class ItemsAdapterEvent extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
     private List<EventItem> items;
     private OnItemClickListener onItemClickListener;
+    final int ORIENTATION_MODE_1 = 1;
+    final int ORIENTATION_MODE_2 = 2;
+    private  String [] month = {"","Января","Февраля","Марта","Апреля","Мая","Июня","Июля","Августа","Сентября","Октября","Ноября","Декабря"};
+    private  String [] month1 = {"","Январь","Февраль","Март","Апрель","Май","Июнь","Июль","Август","Сентябрь","Октябрь","Ноябрь","Декабрь"};
 
     public ItemsAdapterEvent(List<EventItem> items, ItemsAdapterEvent.OnItemClickListener onItemClickListener) {
         this.items = items;
@@ -32,38 +36,87 @@ public class ItemsAdapterEvent extends RecyclerView.Adapter<ItemsAdapterEvent.Vi
     }
 
     @Override
-    public ItemsAdapterEvent.ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
 
-
-        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.fragment_event_row_item, viewGroup, false);
-
-
-
-        return new ViewHolder(view);
-
+        View view;
+        switch (i){
+            case 1:{
+                view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.fragment_event_row_item_orientation_1,viewGroup,false);
+                return new OrientationMode1ViewHolder(view);
+            }
+            case 2:{
+                view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.fragment_event_row_item_orientation_2,viewGroup,false);
+                return new OrientationMode2ViewHolder(view);
+            }
+        }
+        return null;
     }
 
     @Override
-    public void onBindViewHolder(ItemsAdapterEvent.ViewHolder viewHolder, int i) {
+    public int getItemViewType(int position) {
+        final EventItem item = items.get(position);
+        if(item.getOrientationMode() == 1){
+            return ORIENTATION_MODE_1;
+        }
+        else {return ORIENTATION_MODE_2;}
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder viewHolder, int i) {
         final EventItem item = items.get(i);
+        if (viewHolder instanceof OrientationMode1ViewHolder) {
+            OrientationMode1ViewHolder viewHolder1 = (OrientationMode1ViewHolder)viewHolder;
+            viewHolder1.tvWhenEvent_1.setText(item.getEventWhen().substring(8,10));
+            viewHolder1.tvNameEvent_1.setText(item.getName());
+            Picasso.with( viewHolder1.tvImageEvent_1.getContext())
+                    .load(Constants.HTTP.IMAGE_URL + item.getImage().getName()).resize(60,60)
+                    .into( viewHolder1.tvImageEvent_1);
 
-        viewHolder.tv_when_event.setText(item.getEventWhen().substring(0,10));
-        viewHolder.tv_name_event.setText(item.getName());
-        Picasso.with(viewHolder.tv_image_event.getContext()).load(Constants.HTTP.IMAGE_URL +items.get(i).getImage().getPath()).resize(900,500).centerCrop().into(viewHolder.tv_image_event);
+        }else if (viewHolder instanceof  OrientationMode2ViewHolder){
+            OrientationMode2ViewHolder viewHolder2 = (OrientationMode2ViewHolder)viewHolder;
+            viewHolder2.tvNameEvent_2.setText(item.getName());
 
+            for (int k = 0; k <= month.length ; k++){
+                if(item.getEventWhen().substring(5,7).startsWith("0",0)) {
 
+                    if (item.getEventWhen().substring(5, 7).equals("0" + Integer.toString(k))) {
+                        viewHolder2.tvMonthEvent_2.setText(month1[k]);
+                        viewHolder2.tvWhenEvent_2.setText(item.getEventWhen().substring(8, 10) + " " + month[k]);
+                    }
+                }else {
+                    if (item.getEventWhen().substring(5, 7).equals(Integer.toString(k))) {
+                        viewHolder2.tvMonthEvent_2.setText(month1[k]);
+                        viewHolder2.tvWhenEvent_2.setText(item.getEventWhen().substring(8, 10) + " " + month[k]);
 
+                    }
 
-
-        View.OnClickListener listener = new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                onItemClickListener.onItemClick(item);
-
+                }
             }
-        };
-        viewHolder.mView.setOnClickListener(listener);
+            Picasso.with(viewHolder2.tvImageEvent_2.getContext()).load(Constants.HTTP.IMAGE_URL + item.getImage().getPath())
+                    /*.memoryPolicy(MemoryPolicy.NO_CACHE).networkPolicy(NetworkPolicy.NO_CACHE)*/
+                    .resize(500, 300).centerCrop().into(viewHolder2.tvImageEvent_2);
+        }
 
+        if(viewHolder instanceof OrientationMode1ViewHolder) {
+            OrientationMode1ViewHolder viewHolder1 = (OrientationMode1ViewHolder) viewHolder;
+            View.OnClickListener listener = new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    onItemClickListener.onItemClick(item);
+                }
+            };
+            viewHolder1.mView_1.setOnClickListener(listener);
+        }
+        else if (viewHolder instanceof OrientationMode2ViewHolder) {
+            OrientationMode2ViewHolder viewHolder2 = (OrientationMode2ViewHolder) viewHolder;
+            View.OnClickListener listener = new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    onItemClickListener.onItemClick(item);
+                }
+            };
+            viewHolder2.mView_2.setOnClickListener(listener);
+        }
     }
 
     @Override
@@ -71,20 +124,31 @@ public class ItemsAdapterEvent extends RecyclerView.Adapter<ItemsAdapterEvent.Vi
         return items.size();
     }
 
-
-    public class ViewHolder extends RecyclerView.ViewHolder {
-
-        private TextView tv_name_event, tv_description_event,tv_month_event,tv_when_event;
-        private View mView;
-        private ImageView tv_image_event;
-        public ViewHolder(View view) {
+    public class OrientationMode2ViewHolder extends RecyclerView.ViewHolder {
+        private TextView tvNameEvent_2,tvMonthEvent_2,tvWhenEvent_2;
+        private View mView_2;
+        private ImageView tvImageEvent_2;
+        public OrientationMode2ViewHolder(View view) {
             super(view);
-            mView = view.findViewById(R.id.viewEvent);
-            tv_name_event = (TextView) view.findViewById(R.id.tvNameEvent);
-            //tv_month_event = (TextView) view.findViewById(R.id.tvMonthEvent_2);
-           // tv_description_event = (TextView) view.findViewById(R.id.tvShortDescriptionNews_2);
-            tv_when_event = (TextView) view.findViewById(R.id.tvWhenEvent_2);
-            tv_image_event = (ImageView) view.findViewById(R.id.tvImageEvent_2);
+            mView_2 = view.findViewById(R.id.viewEvent_2);
+            tvNameEvent_2 = (TextView) view.findViewById(R.id.tvNameEvent_2);
+            tvMonthEvent_2 = (TextView) view.findViewById(R.id.tvMonthEvent_2);
+            tvWhenEvent_2 = (TextView) view.findViewById(R.id.tvWhenEvent_2);
+            tvImageEvent_2 = (ImageView) view.findViewById(R.id.tvImageEvent_2);
+        }
+    }
+
+    public  class OrientationMode1ViewHolder extends RecyclerView.ViewHolder{
+        private TextView tvNameEvent_1, tvWhenEvent_1,tvMonthEvent_1;
+        private ImageView tvImageEvent_1;
+        private View mView_1;
+
+        public OrientationMode1ViewHolder(View view) {
+            super(view);
+            mView_1 = view.findViewById(R.id.viewEvent_1);
+            tvNameEvent_1 = (TextView) view.findViewById(R.id.tvNameEvent_1);
+            tvImageEvent_1 = (ImageView) view.findViewById(R.id.tvImageEvent_1);
+            tvWhenEvent_1 = (TextView) view.findViewById(R.id.tvWhenEvent_1);
         }
     }
     public interface OnItemClickListener {
