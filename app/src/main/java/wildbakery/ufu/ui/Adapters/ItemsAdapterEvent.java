a26d465158104f651e3db4025e86bf5cad8f7ee2
normeow <rgdar@yandex.ru>
2017-05-14 14:25:55 +0300

    update ItemsAdapterEvent

--- app/src/main/java/wildbakery/ufu/ui/Adapters/ItemsAdapterEvent.java
+++ app/src/main/java/wildbakery/ufu/ui/Adapters/ItemsAdapterEvent.java
@@ -6,6 +6,7 @@ package wildbakery.ufu.ui.Adapters;
 
 import android.support.v7.widget.RecyclerView;
 import android.text.Html;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,34 +19,49 @@ import java.util.List;
 
 import wildbakery.ufu.Constants;
 import wildbakery.ufu.Model.ApiModels.EventItem;
+import wildbakery.ufu.Model.ApiModels.EventItem;
 import wildbakery.ufu.R;
-
-
+import wildbakery.ufu.Utils.PicassoCache;
 
 
 public class ItemsAdapterEvent extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    private static final String TAG = "ItemsAdapterEvent";
     private List<EventItem> items;
-   private   final int ORIENTATION_MODE_1 = 1;
-   private final int ORIENTATION_MODE_2 = 2;
+    private static final int ORIENTATION_MODE_1 = 1;
+    private static final int ORIENTATION_MODE_2 = 2;
+    private static final int PROGRESS_BAR = 3;
 
+    private boolean isLoading;
+    private CallbackListener listener;
 
-    public ItemsAdapterEvent(List<EventItem> items) {
+    // number of item from the end when should be started loading
+    private final int COUNT_OFFSET = 2;
+
+    public interface CallbackListener{
+        void onScrolledToTheEnd();
+    }
+
+    public ItemsAdapterEvent(List<EventItem> items, CallbackListener listener) {
+        this.listener = listener;
         this.items = items;
     }
 
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
 
         View view;
-        switch (i){
-            case 1:{
+        switch (viewType){
+            case ORIENTATION_MODE_1:{
                 view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.fragment_event_row_item_orientation_1,viewGroup,false);
                 return new OrientationMode1ViewHolder(view);
             }
-            case 2:{
+            case ORIENTATION_MODE_2:{
                 view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.fragment_event_row_item_orientation_2,viewGroup,false);
                 return new OrientationMode2ViewHolder(view);
             }
+            case PROGRESS_BAR:
+                view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.progressbar, viewGroup, false);
+                return new ProgressBarViewHolder(view);
         }
         return null;
     }
@@ -53,6 +69,9 @@ public class ItemsAdapterEvent extends RecyclerView.Adapter<RecyclerView.ViewHol
     @Override
     public int getItemViewType(int position) {
         final EventItem item = items.get(position);
+        if (item == null)
+            return PROGRESS_BAR;
+
         if(item.getOrientationMode() == 1){
             return ORIENTATION_MODE_1;
         }
@@ -62,15 +81,22 @@ public class ItemsAdapterEvent extends RecyclerView.Adapter<RecyclerView.ViewHol
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder viewHolder, int i) {
         final EventItem item = items.get(i);
+
+        //scroll almost to the end. notify listener.
+        if (i > items.size() - COUNT_OFFSET){
+            if (!isLoading)
+                listener.onScrolledToTheEnd();
+        }
+
+
         if (viewHolder instanceof OrientationMode1ViewHolder) {
             OrientationMode1ViewHolder viewHolder1 = (OrientationMode1ViewHolder)viewHolder;
             viewHolder1.tvWhenEvent_1.setText(item.getEventWhen().substring(8,10));
             viewHolder1.tvNameEvent_1.setText(item.getName());
-            Picasso.with( viewHolder1.tvImageEvent_1.getContext())
-                    .load(Constants.HTTP.IMAGE_URL + item.getImage().getName()).resize(60,60)
-                    .into( viewHolder1.tvImageEvent_1);
+            PicassoCache.loadImage(item.getImage(),viewHolder1.tvImageEvent_1);
+        }
 
-        }else if (viewHolder instanceof  OrientationMode2ViewHolder){
+        else if (viewHolder instanceof  OrientationMode2ViewHolder){
             OrientationMode2ViewHolder viewHolder2 = (OrientationMode2ViewHolder)viewHolder;
             viewHolder2.tvNameEvent_2.setText(item.getName());
 
@@ -87,16 +113,59 @@ public class ItemsAdapterEvent extends RecyclerView.Adapter<RecyclerView.ViewHol
                     }
                 }
             }
-            Picasso.with(viewHolder2.tvImageEvent_2.getContext()).load(Constants.HTTP.IMAGE_URL + item.getImage().getPath())
-                    /*.memoryPolicy(MemoryPolicy.NO_CACHE).networkPolicy(NetworkPolicy.NO_CACHE)*/
-                    .resize(500, 300).centerCrop().into(viewHolder2.tvImageEvent_2);
+            PicassoCache.loadImage(item.getImage(), viewHolder2.tvImageEvent_2);
             viewHolder2.tvDescription_2.setText(Html.fromHtml(item.getDescription()));
+        }
+
+        else if (viewHolder instanceof ProgressBarViewHolder){
+            ((ProgressBarViewHolder) viewHolder).progressBar.setIndeterminate(true);
         }
     }
 
     @Override
     public int getItemCount() {
         return items.size();
+    }
+
+    public int getActualItemCount(){
+        if (isLoading)
+            return items.size() - 1;
+        return items.size();
+    }
+
+
+    public void add(List<EventItem> items) {
+        try {
+            if (!items.isEmpty()) {
+                this.items.addAll(items);
+                notifyDataSetChanged();
+            }
+        }
+        catch (Exception e){
+            Log.d(TAG, "add: caught");
+            e.printStackTrace();
+        }
+    }
+
+    public void showProgressBar() {Log.d(TAG, "showProgressBar: isLoading = " + isLoading);
+        if (!isLoading) {
+            isLoading = true;
+            items.add(null);
+            try {
+                notifyItemInserted(items.size() - 1);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void hideProgressBar() {
+        Log.d("", "hideProgressBar: isLoading = " + isLoading);
+        if (isLoading){
+            isLoading = false;
+            items.remove(items.size() - 1);
+            notifyItemRemoved(items.size());
+        }
     }
 
     public class OrientationMode2ViewHolder extends RecyclerView.ViewHolder {
@@ -124,5 +193,6 @@ public class ItemsAdapterEvent extends RecyclerView.Adapter<RecyclerView.ViewHol
             tvWhenEvent_1 = (TextView) view.findViewById(R.id.tvWhenEvent_1);
         }
     }
+
 }
 
