df2f95e2bb8382d437c59774c139fae189deeed5
normeow <rgdar@yandex.ru>
2017-05-14 13:15:55 +0300

    add SalesModel

---
+++ app/src/main/java/wildbakery/ufu/Model/SalesModel.java
@@ -0,0 +1,66 @@
+package wildbakery.ufu.Model;
+
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import wildbakery.ufu.Model.ApiModels.SaleItem;
+
+/**
+ * Created by Tatiana on 13/05/2017.
+ */
+
+public class SalesModel {
+    private static final String TAG = "SalesModel";
+
+    private List<SaleItem> items = new ArrayList<>();
+
+    private static SalesModel instanse;
+
+    private SalesModel(){}
+
+    public static SalesModel getInstanse() {
+        if (instanse == null)
+            instanse = new SalesModel();
+        return instanse;
+    }
+
+    public List<SaleItem> getItems() {
+        // returns copy of data
+        return new ArrayList<>(items);
+    }
+
+    /**
+     *
+     * @param count count of items in batch
+     * @return last items in list
+     */
+
+    public List<SaleItem> getBatchItems(int start, int count){
+        if (items.isEmpty())
+            return null;
+        int end = (start + count) < items.size() ? start + count : items.size();
+        Log.d("TEST", "getBatchItems: start = " + start + " end = " + end);
+        List<SaleItem> subl = items.subList(start, end);
+        return subl;
+    }
+
+    public void setItems(List<SaleItem> items) {
+        Log.d(TAG, "setItems");
+        this.items = items;
+    }
+
+    public void addItems(List<SaleItem> items) {
+        if (items == null)
+            return;
+        if (this.items == null)
+            this.items = new ArrayList<>();
+        this.items.addAll(items);
+    }
+
+    public void clearModel(){
+        Log.d(TAG, "clearModel: ");
+        items = new ArrayList<>();
+    }
+}
