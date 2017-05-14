dcc5f10fec1d2ec212f653f88873587bab12343e
normeow <rgdar@yandex.ru>
2017-05-14 13:11:54 +0300

    update DatabaseHelper with getSalesDAO(), getEventsDAO()

--- app/src/main/java/wildbakery/ufu/Model/DatabaseHelper.java
+++ app/src/main/java/wildbakery/ufu/Model/DatabaseHelper.java
@@ -12,15 +12,21 @@ import com.j256.ormlite.table.TableUtils;
 
 import java.sql.SQLException;
 
+import wildbakery.ufu.Model.ApiModels.EventItem;
 import wildbakery.ufu.Model.ApiModels.Image;
 import wildbakery.ufu.Model.ApiModels.JobItem;
 import wildbakery.ufu.Model.ApiModels.NewsItem;
+import wildbakery.ufu.Model.ApiModels.SaleItem;
+import wildbakery.ufu.Model.DAO.EventsDAO;
+import wildbakery.ufu.Model.DAO.EventsDAOImpl;
 import wildbakery.ufu.Model.DAO.ImageDAO;
 import wildbakery.ufu.Model.DAO.ImageDAOImpl;
 import wildbakery.ufu.Model.DAO.JobsDAO;
 import wildbakery.ufu.Model.DAO.JobsDAOImpl;
 import wildbakery.ufu.Model.DAO.NewsDAO;
 import wildbakery.ufu.Model.DAO.NewsDAOImpl;
+import wildbakery.ufu.Model.DAO.SalesDAO;
+import wildbakery.ufu.Model.DAO.SalesDAOImpl;
 
 /**
  * Created by Tatiana on 28/04/2017.
@@ -34,6 +40,8 @@ public class DatabaseHelper extends OrmLiteSqliteOpenHelper {
     private NewsDAO newsDao;
     private ImageDAO imageDAO;
     private JobsDAO jobsDAO;
+    private SalesDAO salesDAO;
+    private EventsDAO eventsDAO;
 
     public DatabaseHelper(Context context){
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -45,6 +53,8 @@ public class DatabaseHelper extends OrmLiteSqliteOpenHelper {
             TableUtils.createTable(connectionSource, NewsItem.class);
             TableUtils.createTable(connectionSource, Image.class);
             TableUtils.createTable(connectionSource, JobItem.class);
+            TableUtils.createTable(connectionSource, SaleItem.class);
+            TableUtils.createTable(connectionSource, EventItem.class);
         } catch (SQLException e) {
             Log.e(DatabaseHelper.class.getName(), "Unable to create datbases", e);
         }
@@ -56,6 +66,8 @@ public class DatabaseHelper extends OrmLiteSqliteOpenHelper {
             TableUtils.dropTable(connectionSource, NewsItem.class, true);
             TableUtils.dropTable(connectionSource, Image.class, true);
             TableUtils.dropTable(connectionSource, JobItem.class, true);
+            TableUtils.dropTable(connectionSource, SaleItem.class, true);
+            TableUtils.dropTable(connectionSource, EventItem.class, true);
             onCreate(sqLiteDatabase, connectionSource);
 
         } catch (SQLException e) {
@@ -80,6 +92,18 @@ public class DatabaseHelper extends OrmLiteSqliteOpenHelper {
         if (jobsDAO == null)
             jobsDAO = new JobsDAOImpl(getConnectionSource(), JobItem.class);
         return jobsDAO;
+    }
+
+    public SalesDAO getSalesDAO() throws SQLException {
+        if (salesDAO == null)
+            salesDAO = new SalesDAOImpl(getConnectionSource(), SaleItem.class);
+        return salesDAO;
+    }
+
+    public EventsDAO getEventsDAO() throws SQLException {
+        if (eventsDAO == null)
+            eventsDAO = new EventsDAOImpl(getConnectionSource(), EventItem.class);
+        return  eventsDAO;
     }
 
 
